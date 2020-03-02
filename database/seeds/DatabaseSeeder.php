9ae9dbed7b5f825da34546dff41c1f949c570f46
Dmitry <dima6750@yandex.ru>
2020-03-02 18:23:08 +0500

    Update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(BranchesTableSeeder::class);
+        $this->call(CalendarsTableSeeder::class);
+        $this->call(ClientsTableSeeder::class);
+        $this->call(PaymentsTableSeeder::class);
+        $this->call(RoomsTableSeeder::class);
     }
 }
