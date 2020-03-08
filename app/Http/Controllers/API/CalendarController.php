f9126f397313c1b9958cf82fa96d6b2d4b95851c
Dmitry <dima6750@yandex.ru>
2020-03-08 19:53:22 +0500

    Update

--- app/Http/Controllers/API/CalendarController.php
+++ app/Http/Controllers/API/CalendarController.php
@@ -22,7 +22,9 @@ class CalendarController extends BaseController
 
         if ($request->has('room')) {
             $roomId = $request->input('room');
-            $calendars = Calendar::where('room', $roomId);
+            $calendars = Calendar::with(['room' => function($q) use ($roomId) {
+                $q->where('id', '=', $roomId);
+            }]);
         }
         $respone = new CalendarCollection($calendars);
         return $this->sendResponse($respone, 'Success Ok');
