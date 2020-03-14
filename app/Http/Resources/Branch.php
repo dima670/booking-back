b4b2cebcb91de0ad66d22d59767077fcffaa6b1a
Dmitry <dima6750@yandex.ru>
2020-03-14 20:15:48 +0500

    Update

--- app/Http/Resources/Branch.php
+++ app/Http/Resources/Branch.php
@@ -3,7 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Http\Resources\RoomCollection;
+use App\Http\Resources\Room as RoomResource;
 
 class Branch extends JsonResource
 {
@@ -19,9 +19,9 @@ class Branch extends JsonResource
             'id' => $this->id,
             'name' => $this->branch_name,
             'address' => $this->address,
-            'work_from' => $this->word_from,
+            'work_from' => $this->work_from,
             'work_to' => $this->work_to,
-//            'rooms' => new RoomCollection($this->load('rooms')),
+            'rooms' => RoomResource::collection($this->load('rooms')),
         ];
     }
 }
