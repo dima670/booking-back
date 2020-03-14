bc55acd0883a07524e63e51048043681a5f19402
Dmitry <dima6750@yandex.ru>
2020-03-14 20:21:23 +0500

    Update

--- app/Http/Resources/Branch.php
+++ app/Http/Resources/Branch.php
@@ -21,7 +21,7 @@ class Branch extends JsonResource
             'address' => $this->address,
             'work_from' => $this->work_from,
             'work_to' => $this->work_to,
-            'rooms' => new RoomResource($this->load('rooms')),
+            'rooms' => new RoomCollection($this->load('rooms')),
         ];
     }
 }
