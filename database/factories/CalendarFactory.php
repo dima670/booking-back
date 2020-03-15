25a212d590f4fe2b29b40cefaf537edafc15d482
Dmitry <dima6750@yandex.ru>
2020-03-15 16:23:29 +0500

    Update

--- database/factories/CalendarFactory.php
+++ database/factories/CalendarFactory.php
@@ -8,8 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Calendar::class, function (Faker $faker) {
     return [
         'room_id' => $faker->numberBetween(1, 10),
-        'time_from' => $faker->dateTimeBetween($startDate = 'now', $endDate = '+1 day', null),
-        'time_to' => $faker->dateTimeBetween($startDate = '+1 hour', $endDate = '+1 day', null),
+        'time_from' => $faker->dateTimeBetween($startDate = 'now', $endDate = '+1 hour', null),
+        'time_to' => $faker->dateTimeBetween($startDate = '+2 hour', $endDate = '+3 hour', null),
         'client_id' => $faker->numberBetween(1, 50),
         'user_id' => $faker->numberBetween(1, 1),
         'payment_id' => $faker->numberBetween(1, 150),
