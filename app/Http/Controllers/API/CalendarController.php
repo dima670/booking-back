95d7007c6068b17667a57f0e03a802d4f401018c
Dmitry <dima6750@yandex.ru>
2020-03-18 16:55:59 +0500

    Update

--- app/Http/Controllers/API/CalendarController.php
+++ app/Http/Controllers/API/CalendarController.php
@@ -49,7 +49,7 @@ class CalendarController extends BaseController
      */
     public function show(Calendar $calendar)
     {
-        $calendar = $calendar->with(['payment', 'client'])->get();
+        $calendar = $calendar->load(['payment', 'client'])->get();
         return $this->sendResponse(new CalendarResource($calendar), 'Success Ok');
     }
 
