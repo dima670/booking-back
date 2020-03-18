be0617faa86582577802d81582decbe987967766
Dmitry <dima6750@yandex.ru>
2020-03-18 17:14:03 +0500

    Update

--- app/Http/Controllers/API/CalendarController.php
+++ app/Http/Controllers/API/CalendarController.php
@@ -62,7 +62,7 @@ class CalendarController extends BaseController
      */
     public function update(Request $request, Calendar $calendar)
     {
-        $calendar->update($request->all());
+        $calendar->fill($request->all())->save();
 
         $response = $calendar->load(['payment', 'client']);
         return $this->sendResponse(new CalendarResource($response), 'Success Ok');
